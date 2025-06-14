40faec02e771077baefdbb30a0ef8cea91338103
AbdKaan <abdu.kaan@gmail.com>
2025-06-14 12:14:56 +0300

    Get file content

--- tests.py
+++ tests.py
@@ -1,7 +1,10 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+
+# from functions.get_files_info import get_files_info
 
 
 def test():
+    """
     result = get_files_info("calculator", ".")
     print("Result for current directory:")
     print(result)
@@ -18,6 +21,37 @@ def test():
     result = get_files_info("calculator", "../")
     print("Result for '../' directory:")
     print(result)
+
+    print()
+    """
+    result = get_file_content("calculator", "lorem.txt")
+    print("Result for 'lorem.txt':")
+    print("Length should be 10000 + info's length:")
+    print(f"Length: {len(result)}")
+    print("File content:")
+    print(result)
+    print()
+
+    result = get_file_content("calculator", "main.py")
+    print("Result for 'main.py':")
+    print(f"Length: {len(result)}")
+    print("File content:")
+    print(result)
+    print()
+
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print("Result for 'pkg/calculator.py':")
+    print(f"Length: {len(result)}")
+    print("File content:")
+    print(result)
+    print()
+
+    result = get_file_content("calculator", "/bin/cat")
+    print("Result for '/bin/cat':")
+    print(f"Length: {len(result)}")
+    print("File content:")
+    print(result)
+    print()
 
 
 if __name__ == "__main__":
